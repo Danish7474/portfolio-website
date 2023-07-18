5abd5bf83bde67910c6ffb061280dca00c71a4a0
Danish7474 <123730280+Danish7474@users.noreply.github.com>
2023-07-18 12:28:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector('.cros').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo')
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display = 'inline'
+        document.querySelector('.cros').style.display = 'none'
+    }
+    else{
+        
+        document.querySelector('.ham').style.display = 'none'
+        setTimeout(()=>{
+            document.querySelector('.cros').style.display = 'inline'
+        },200)
+        
+    }
+})
